7f45686d759bd2073f03d54c764f12557e08d853
Cheetah's Creations <sleek.cheetah@gmail.com>
2026-05-25 16:17:26 -0400

    Update __init__.py

--- variables/__init__.py
+++ variables/__init__.py
@@ -1,13 +1,15 @@
-# Mark the variables directory as a Python package
-# Expose primary simulation hooks for easy top-level access
+"""
+CHTS GEN-III Variable and Controller Package.
+Provides interface for thermodynamic nodes, thermal buffers, 
+and cascaded zeotropic management.
+"""
 
-from .variable_theory_testing import calculate_hartmann_drag, evaluate_rf_ionization_overhead
-from .teg_sandwich import simulate_teg_core
-from .zeotropic_mix import calculate_zeotropic_glide
+from .GEN_III_node_amplifiers import CHTSController
+from .GEN_III_thermal_buffer import ThermalBuffer
+from .GEN_III_zeotropic_mix import ZeotropicManager
 
 __all__ = [
-    "calculate_hartmann_drag",
-    "evaluate_rf_ionization_overhead",
-    "simulate_teg_core",
-    "calculate_zeotropic_glide",
+    "CHTSController",
+    "ThermalBuffer",
+    "ZeotropicManager"
 ]
