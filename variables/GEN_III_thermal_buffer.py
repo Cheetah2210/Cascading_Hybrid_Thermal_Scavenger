2938765481e514f2234adde5ce4244ef7dcd12f6
Cheetah's Creations <sleek.cheetah@gmail.com>
2026-05-25 16:26:31 -0400

    Update GEN_III_thermal_buffer.py

--- variables/GEN_III_thermal_buffer.py
+++ variables/GEN_III_thermal_buffer.py
@@ -5,5 +5,6 @@ class ThermalBuffer:
         self.current_stored_heat = 0.0
 
     def update(self, heat_flux_kw, delta_t_sec):
+        """Updates buffer state based on incoming flux."""
         self.current_stored_heat += (heat_flux_kw * delta_t_sec)
         return min(self.current_stored_heat, self.capacity_kj)
