2a1aa12cdca06e1b4e66477ec14725a287f8b86f
Cheetah's Creations <sleek.cheetah@gmail.com>
2026-05-25 00:12:40 -0400

    Create conftest.py

---
+++ tests/conftest.py
@@ -0,0 +1,6 @@
+The-Cascading-Hybrid-Thermal-Scavenger/
+├── variables/
+├── tests/
+│   ├── conftest.py       <-- Add this (can be completely blank)
+│   ├── test_cascades.py
+│   └── test_mhd_core.py
